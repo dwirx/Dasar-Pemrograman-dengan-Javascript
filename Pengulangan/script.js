ed671ead5aabc22533a461ab260e46fb4d2cf5d2
hendrabangun <bangundwir@gmail.com>
2020-11-10 19:18:04 +0700

    pengkondisina dan pengulangan

--- Pengulangan/script.js
+++
@@ -1,5 +0,0 @@
-var nilaiAwal = 1;
-while (nilaiAwal <= 100) {
-  console.log("Hello World" + nilaiAwal + "x");
-  nilaiAwal++;
-}
--- code/script.js
+++ code/script.js
@@ -5,7 +5,7 @@
 // }
 
 // // s = string
-// //Contoh 2
+// //Contoh 2 dari atas ke bawah
 // var s = '';
 // for( var i = 0; i < 10; i++ ){
 //     // s += '*'; membuat jumlah baris
@@ -17,14 +17,37 @@
 
 // console.log(s);
 
-//contoh 3
-var s = '';
-for( var i = 10; i > 0; i-- ){
-    // s += '*'; membuat jumlah baris
-    for( var j = 0; j < i; j++ ) {
-        s += '*';
-    }
-    s += '\n';
-}
+// //contoh 3 dari bawah ke atas
+// var s = '';
+// for( var i = 10; i > 0; i-- ){
+//     // s += '*'; membuat jumlah baris
+//     for( var j = 0; j < i; j++ ) {
+//         s += '*';
+//     }
+//     s += '\n';
+// }
 
-console.log(s);
+// console.log(s);
+
+// //contoh 4 
+// var s = '';
+// for (var i = 7; i >= 1; i--) {
+//   var s = "";
+//   for (var j = i; j <= 7; j++) {
+//   s += "*";
+//      }
+//  console.log(s);
+// }
+
+var a = '';
+var n = 5;
+var m = (n-1); 
+for(i=1; i <= n; i++)
+{
+    a = a.trim();
+    a = ' '.repeat(m) + a + (i > 1 ? ' ' : '') + '*';
+    console.log(a);
+    m--;
+}
+    
+
